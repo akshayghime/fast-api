5b7fd1c2967b4f3c6191b53b4cf211115efea651
akshayghime <55523567+akshayghime@users.noreply.github.com>
2025-01-21 17:51:56 +0000

    Adding changes to main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import wiki_search
+from mylib.logic import wiki as wikilogic
 
-print(wiki())
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API Call/Search wiki"}
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+
+    result = wiki_search(value)
+    return {"result": result}
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    """Retrieve wikipedia page"""
+
+    result = wikilogic(name)
+    return {"result": result}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host='0.0.0.0')
